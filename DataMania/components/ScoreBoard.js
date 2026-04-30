794bd13ecab7f977c0184a0e43f900e40c00e1d5
Aaron-Paranoid-Android <aaford01@wsc.edu>
2026-04-30 12:33:45 -0500

    number validation

--- DataMania/components/ScoreBoard.js
+++ DataMania/components/ScoreBoard.js
@@ -1,2 +1,12 @@
-let x = 0;
-let y = 0;
+function scoreBoard() {
+  // Get the value of the input field with id="numb"
+  let x = document.getElementById("score1", "score2").value;
+  // If x is Not a Number or less than one or greater than 10
+  let text;
+  if (isNaN(x) || x < 100 || x > 100000) {
+    text = "Input not valid";
+  } else {
+    text = "Input OK";
+  }
+  document.getElementById("demo").innerHTML = text;
+}
