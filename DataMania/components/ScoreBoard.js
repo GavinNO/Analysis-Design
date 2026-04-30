4822132391adc74250057a80cb19e2b85f9aca0e
Aaron-Paranoid-Android <aaford01@wsc.edu>
2026-04-30 12:40:33 -0500

    show scores

--- DataMania/components/ScoreBoard.js
+++ DataMania/components/ScoreBoard.js
@@ -1,12 +1,18 @@
 function scoreBoard() {
   // Get the value of the input field with id="numb"
-  let x = document.getElementById("score1", "score2").value;
-  // If x is Not a Number or less than one or greater than 10
+  let s1 = document.getElementById("score1").value;
+  let s2 = document.getElementById("score1").value;
+  // If the two scores are greater than 100 or less than 100,000
   let text;
-  if (isNaN(x) || x < 100 || x > 100000) {
+  if (isNaN(s1) || x < 100 || x > 100000) {
     text = "Input not valid";
   } else {
-    text = "Input OK";
+    text = s1;
+  }
+  if (isNaN(s2) || x < 100 || x > 100000) {
+    text = "Input not valid";
+  } else {
+    text = s2;
   }
   document.getElementById("demo").innerHTML = text;
 }
